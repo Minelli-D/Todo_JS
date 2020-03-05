ef459b0fe8138cd9ff66fea5e1ce67e6062ef6c7
Minelli-D <davide@minelli.io>
2020-03-05 21:13:12 +0100

    refactor add to container method

--- src/cards.js
+++ src/cards.js
@@ -1,3 +1,9 @@
+function append_to_element(array,e_append){
+    array.forEach(element => {
+        e_append.appendChild(element)
+    });
+}
+
 function card_head(){
     let div_container = document.createElement('div')
     div_container.style.cssText = "display:flex;"
@@ -9,6 +15,42 @@ function card_head(){
 
     div_container.appendChild(div_head)
     return div_container
+}
+
+function card_input(){
+    let div_container = document.createElement('div')
+    let div_container_input = document.createElement('div')
+    let div_span = document.createElement('span')
+    div_span.textContent = "span"
+    let div_input = document.createElement('input')
+
+    div_container_input.style.cssText = "display:flex;"
+    div_span.style.cssText = "display:flex; flex:1;"
+    div_input.style.cssText = "display:flex; flex:2;"
+
+    div_container_input.appendChild(div_span)
+    div_container_input.appendChild(div_input)
+    div_container.appendChild(div_container_input)
+    return div_container
+
+}
+
+function card_content(){
+    let div_container = document.createElement('div')
+    let div_container_input = document.createElement('div')
+    let div_span = document.createElement('span')
+    div_span.textContent = "span"
+    let div_content = document.createElement('div')
+
+    div_container_input.style.cssText = "display:flex;"
+    div_span.style.cssText = "display:flex; flex:1; justify-content:center;"
+    div_content.style.cssText = "display:flex; flex:2; border: 1px solid; border-radius:10px"
+
+    div_container_input.appendChild(div_span)
+    div_container_input.appendChild(div_content)
+    div_container.appendChild(div_container_input)
+    return div_container
+
 }
 
 function card_body(){
@@ -26,16 +68,11 @@ function card_body(){
 }
 
 
-function append_to_container(array){
-    array.forEach(element => {
-        container[0].appendChild(element)
-    });
-}
-
-
 function card_composition(){
     let array = new Array()
     array.push(card_head())
     array.push(card_body())
-    append_to_container(array)
+    array.push(card_input())
+    array.push(card_content())
+    append_to_element(array,container[0])
 }
