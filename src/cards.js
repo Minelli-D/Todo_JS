fd8bac32bbf00da2a522628dac1cf6e4dd1abb37
Minelli-D <davide@minelli.io>
2020-03-05 21:26:59 +0100

    some features

--- src/cards.js
+++ src/cards.js
@@ -4,13 +4,13 @@ function append_to_element(array,e_append){
     });
 }
 
-function card_head(){
+function card_head(string){
     let div_container = document.createElement('div')
     div_container.style.cssText = "display:flex;"
 
     let div_head = document.createElement('div')
     div_head.style.cssText = "display:flex; justify-content:center; flex:1;"
-    div_head.textContent = 'TITLE'
+    div_head.textContent = string
 
 
     div_container.appendChild(div_head)
@@ -35,16 +35,19 @@ function card_input(){
 
 }
 
-function card_content(){
+function card_content(span,content){
     let div_container = document.createElement('div')
     let div_container_input = document.createElement('div')
-    let div_span = document.createElement('span')
-    div_span.textContent = "span"
-    let div_content = document.createElement('div')
 
+    let div_span = document.createElement('span')
+    div_span.textContent = span
+
+    let div_content = document.createElement('div')
+    div_content.textContent=content
+    
     div_container_input.style.cssText = "display:flex;"
     div_span.style.cssText = "display:flex; flex:1; justify-content:center;"
-    div_content.style.cssText = "display:flex; flex:2; border: 1px solid; border-radius:10px"
+    div_content.style.cssText = "display:flex; flex:2; justify-content:center; border: 1px solid; border-radius:10px"
 
     div_container_input.appendChild(div_span)
     div_container_input.appendChild(div_content)
@@ -61,18 +64,20 @@ function card_body(){
     div_head.style.cssText = "display:flex; flex:1;"
     div_head.textContent = 'body'
 
-
     div_container.appendChild(div_head)
     return div_container
 
 }
 
 
-function card_composition(){
+function card_composition(task){
     let array = new Array()
-    array.push(card_head())
+    let box = document.createElement('div')
+    box.setAttribute('class','edit_task scale-up-hor-right')
+    array.push(card_head('Task Type'))
     array.push(card_body())
     array.push(card_input())
-    array.push(card_content())
-    append_to_element(array,container[0])
+    array.push(card_content('span','a'))
+    append_to_element(array,box)
+    container[0].appendChild(box)
 }
